8921cb41ff0902ce6fbd4634de9e5f166ee78748
Manish KC <kcmanish89@gmail.com>
2019-08-16 12:53:23 -0700

    Assignment complete

---
+++ src/main/java/quotes/onlineQuote.java
@@ -0,0 +1,12 @@
+package quotes;
+
+public class onlineQuote {
+String quoteText;
+String quoteAuthor;
+
+    public onlineQuote(String quoteText, String quoteAuthor) {
+        this.quoteText = quoteText;
+        this.quoteAuthor = quoteAuthor;
+    }
+
+}
